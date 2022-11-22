997cb6614140d71fdc32675b799d67fa446466d9
GioDgebuadze15 <gio.dgebuadze5@gmail.com>
2022-11-22 12:12:52 +0400

    build yaml

---
+++ web-app/pages/index.vue
@@ -0,0 +1,16 @@
+﻿<template>
+<div>
+  <v-text-field label="First Name"/>
+  <v-text-field label="Last Name"/>
+</div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
